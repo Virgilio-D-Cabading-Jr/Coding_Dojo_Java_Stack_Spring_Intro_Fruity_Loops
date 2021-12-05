cbfcced70cd8dc854378799bddcf1254b32a27c2
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-05 00:14:06 -0800

    add ItemController

---
+++ src/main/java/com/vcabading/fruityloops/controllers/ItemController.java
@@ -0,0 +1,28 @@
+package com.vcabading.fruityloops.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/////////////////////////////////////////////////////////////////
+//HOME CONTROLLER
+/////////////////////////////////////////////////////////////////
+
+@Controller
+public class ItemController {
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		//	//// Create Fruits //////////////////////////////////
+        ArrayList<Item> fruits = new ArrayList<Item>();
+        fruits.add(new Item("Kiwi", 1.5));
+        fruits.add(new Item("Mango", 2.0));
+        fruits.add(new Item("Goji Berries", 4.0));
+        fruits.add(new Item("Guava", .75));
+        
+        //	//// Load attributes to the Model ///////////////////
+        model.addAttribute("fruits", fruits);
+		
+		return "index.jsp";
+	}
+}
